5ad187ad28464986ab69016f1cb49b303c616bae
HP <prs2418@gmail.com>
2021-03-10 16:41:53 +0530

    Revert "hello from your friend Royston"
    This reverts commit f2f9918d621440c3bc6727a191c6ff69c43d835f.

--- app/src/main/java/com/devxnow/githubtesting/MainActivity.java
+++ app/src/main/java/com/devxnow/githubtesting/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText ( this, "hello i am pawana", Toast.LENGTH_SHORT ).show ();
-        My name is royston;
+
+        i will do mistake by making mistake;
     }
 }
