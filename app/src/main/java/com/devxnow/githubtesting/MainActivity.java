93eb725a0c33d288c94590fab5f832ec28bc27d0
DevX Now <80193535+DevelopersDevXnow@users.noreply.github.com>
2021-03-12 03:40:24 +0530

    commit Royston sanctis branchbranch 01

--- app/src/main/java/com/devxnow/githubtesting/MainActivity.java
+++ app/src/main/java/com/devxnow/githubtesting/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText ( this, "hello i am pawana", Toast.LENGTH_SHORT ).show ();
+        Toast.makeText ( this, "hello i am Royston ", Toast.LENGTH_SHORT ).show ();
 
         Toast.makeText ( this, "Working with branchbranch01", Toast.LENGTH_SHORT ).show ();
 
